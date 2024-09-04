e9a779628a6676f9b555bf98ae3eed0ab035ceca
Aaron Staples <amstaples25@gmail.com>
2024-09-04 09:29:13 -0500

    Outline for Loop

--- pkfMain.py
+++ pkfMain.py
@@ -1,1 +1,9 @@
 #Hello World
+
+
+#Perpetual While loop to constantly run?
+
+#If no shiny or buttons for battle (In Overworld) Be in Walk Loop
+#If no shiny but Buttons for battle are on screen try to find shiny. If no shiny Run from battle, return to walk loop.
+#If yes shiny pay attention to where it is and pick up pixels hexideciaml to verify the color to sus out false positives
+#If still shiny end program our job is done. If not shiny (Failed Verification) run from the battle. Return to Walk Loop
